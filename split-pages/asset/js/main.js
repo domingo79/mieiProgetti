6761f8370d5eaa46c50f83bb4fe79b9371850f20
domingo79 <andria140779@yahoo.it>
2021-06-27 16:02:14 +0200

    event v-2.0

--- split-pages/asset/js/main.js
+++ split-pages/asset/js/main.js
@@ -3,8 +3,8 @@ const right = document.querySelector('.right');
 const container = document.querySelector('.container');
 
 
-// 1- versione macchinosa ma funzionante
-
+// v-1.0 versione macchinosa ma funzionante
+/*
 left.addEventListener('mouseenter', () => {
     container.classList.remove('hover_right');
     container.classList.add('hover_left');
@@ -14,3 +14,11 @@ right.addEventListener('mouseenter', () => {
     container.classList.remove('hover_left');
     container.classList.add('hover_right');
 });
+*/
+
+// v-2.0
+left.addEventListener('mouseenter', () => container.classList.add('hover_left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover_left'));
+
+right.addEventListener('mouseenter', () => container.classList.add('hover_right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover_right'));
